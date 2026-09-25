cee660f9596177004a1a4609c8d5d7c6825909fe
Sound-ofSilence <2591624792@qq.com>
2026-09-26 00:30:49 +0800

    feat: finalize v1.0.0, configure base path and GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  base: '/dspgame/', // 必须与你的 GitHub 仓库名完全一致
+});
